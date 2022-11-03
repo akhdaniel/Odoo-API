12e9e7751856dc157ec09c823a2d4585ab7851b4
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2022-11-03 14:00:22 +0700

    Update python-rest.py

--- python/python-rest.py
+++ python/python-rest.py
@@ -1,14 +1,10 @@
 import requests
 
-# r = requests.get('http://localhost:8069/vit_api/list/res.partner')
-# print(r.text)
+r = requests.get('http://localhost:8069/vit_api/list/res.partner')
+print(r.text)
 
-
-
-# r = requests.get('http://localhost:8069/vit_api/read/res.partner/10')
-# print(r.text)
-
-
+r = requests.get('http://localhost:8069/vit_api/read/res.partner/10')
+print(r.text)
 
 r = requests.post('http://localhost:8069/vit_api/create/res.partner',
         data={'data': '{"name":"Test API"}'}
